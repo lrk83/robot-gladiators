5b02174a7fce279ac97c8d6aac17b4dc8708093d
lrk83@cornell.edu <lukas@mac.lan>
2021-05-20 17:41:35 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,13 +4,19 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
-var enemyHealth = 50;
+var enemyHealth = randomNumber(40,60);
 var enemyAttack = 12;
 
 console.log(enemyNames);
 console.log(enemyNames.length);
 console.log(enemyNames[0]);
 console.log(enemyNames[3]);
+
+//generate a random numeric value
+var randomNumber = function(min, max){
+  var value = Math.floor(Math.random()*(max-min+1))+min;
+  return value;
+}
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
@@ -27,14 +33,15 @@ var fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0,playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack-3, playerAttack);
+    enemyHealth = Math.max(0,enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -53,7 +60,8 @@ var fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack-3, enemyAttack);
+    playerHealth = Math.max(0,playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
